c98bb367016c6f7a809b53c25c09fc3bf1adf916
so1icitx <tojitiktok@proton.me>
2025-03-06 15:28:49 +0200

    Update analyzer.cs

--- analyzer.cs
+++ analyzer.cs
@@ -1,1 +1,5 @@
-Console.WriteLine("wassup");
+Console.WriteLine("[!] Welcome to The Sigma Txt Analyzer");
+Console.WriteLine("Enter your text:");
+string userText = Console.ReadLine();
+
+Console.WriteLine(userText);
