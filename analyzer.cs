f99cc58a907d90623920122916d1e27712c47314
so1icitx <tojitiktok@proton.me>
2025-03-09 08:10:01 +0200

    Update analyzer.cs

--- analyzer.cs
+++ analyzer.cs
@@ -7,3 +7,26 @@ if (text == "")
     Console.WriteLine("[!] ERROR: No text provided. Exiting...");
     return;
 }
+
+while (true)
+{
+    ShowMenu();
+    string pick = Console.ReadLine();
+
+    if (pick == "5")
+    {
+        Console.WriteLine("Sayonara!");
+        break;
+    }
+}
+
+void ShowMenu()
+{
+    Console.WriteLine("\nOptions:");
+    Console.WriteLine("1. Word Count");
+    Console.WriteLine("2. Mood Check");
+    Console.WriteLine("3. Text Stats");
+    Console.WriteLine("4. Word Lengths");
+    Console.WriteLine("5. Exit");
+    Console.Write("Pick an option (1-5): ");
+}
