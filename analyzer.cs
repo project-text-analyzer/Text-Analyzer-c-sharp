41e3208f9dec99737b914543562b6543dad4d00f
so1icitx <tojitiktok@proton.me>
2025-03-18 18:36:05 +0200

    bug fixes + simplier code

--- analyzer.cs
+++ analyzer.cs
@@ -1,42 +1,28 @@
+using System;
 using System.Linq;
 
+
+string asciiLogo = """""
+
+                   .dP"Y8 88  dP""b8 8b    d8    db        888888 Yb  dP 888888        db    88b 88    db    88         Yb  dP 8888P 888888 88""Yb 
+                   `Ybo." 88 dP   `" 88b  d88   dPYb         88    YbdP    88         dPYb   88Yb88   dPYb   88          YbdP    dP  88__   88__dP 
+                   o.`Y8b 88 Yb  "88 88YbdP88  dP__Yb        88    dPYb    88        dP__Yb  88 Y88  dP__Yb  88  .o       8P    dP   88""   88"Yb  
+                   8bodP' 88  YboodP 88 YY 88 dP""""Yb       88   dP  Yb   88       dP""""Yb 88  Y8 dP""""Yb 88ood8      dP    d8888 888888 88  Yb 
+
+                   """"";
+
+Console.WriteLine(asciiLogo);
 Console.WriteLine("[*] Welcome to The Sigma Txt Analyzer");
 Console.WriteLine("Enter your text:");
 string text = Console.ReadLine();
 
-if (text == "")
+if (string.IsNullOrWhiteSpace(text))
 {
     Console.WriteLine("[!] ERROR: No text provided. Exiting...");
     return;
 }
 
 while (true)
-{
-    ShowMenu();
-    string pick = Console.ReadLine();
-
-    if (pick == "5")
-    {
-        Console.WriteLine("Sayonara!");
-        break;
-    }
-    while (pick != "1" && pick != "2" && pick != "3" && pick != "4" && pick != "5")
-            {
-                Console.WriteLine("[!] Invalid input, please pick a number between 1 and 5.");
-                Console.WriteLine("Try again? (y/n): ");
-                string tryAgain = Console.ReadLine().ToLower();
-                if (tryAgain != "y")
-                {
-                    Console.WriteLine("Sayonara!");
-                    return; 
-                }
-                ShowMenu();
-                pick = Console.ReadLine();
-            }
-    RunOption(pick);
-}
-
-void ShowMenu()
 {
     Console.WriteLine("\nOptions:");
     Console.WriteLine("1. Word Count");
@@ -45,125 +31,110 @@ void ShowMenu()
     Console.WriteLine("4. Word Lengths");
     Console.WriteLine("5. Exit");
     Console.Write("Pick an option (1-5): ");
-}
-void RunOption(string pick)
-{
+
+    string pick = Console.ReadLine();
+
     switch (pick)
     {
         case "1":
-            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             Console.WriteLine("Word Count: " + words.Length);
             break;
+
         case "2":
-            string[] textWords = text.ToLower().Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-                    
-                    // Positive words list
-                    string[] positiveWords = new string[] {
-                        "happy", "great", "awesome", "good", "excellent", "wonderful", "amazing",
-                        "beautiful", "love", "joy", "fantastic", "perfect", "nice", "lovely",
-                        "exciting", "brilliant", "super", "cool", "fun", "glad", "positive",
-                        "success", "win", "cheerful", "delight", "pleasure", "hope"
-                    };
+            string[] positiveWords = { "happy", "good", "great", "awesome", "love" };
+            string[] negativeWords = { "sad", "bad", "terrible", "hate", "awful" };
 
-                    // Negative words list
-                    string[] negativeWords = new string[] {
-                        "sad", "bad", "terrible", "awful", "horrible", "depressing", "angry",
-                        "hate", "miserable", "poor", "dreadful", "pain", "sucks", "stupid",
-                        "ugly", "boring", "annoying", "mad", "furious", "fail", "negative",
-                        "stress", "anxiety", "fear", "disappoint", "upset", "hurt"
-                    };
+            int positiveCount = 0;
+            int negativeCount = 0;
 
-                    int positiveCount = 0;
-                    int negativeCount = 0;
+            string[] textWords = text.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-                    foreach (string word in textWords)
-                    {
-                        if (positiveWords.Contains(word))
-                            positiveCount++;
-                        if (negativeWords.Contains(word))
-                            negativeCount++;
-                    }
-
-                    Console.WriteLine("Mood Analysis:");
-                    Console.WriteLine($"Positive words found: {positiveCount}");
-                    Console.WriteLine($"Negative words found: {negativeCount}");
-                    
-                    if (positiveCount > negativeCount)
-                        Console.WriteLine("Overall mood: Positive");
-                    else if (negativeCount > positiveCount)
-                        Console.WriteLine("Overall mood: Negative");
-                    else
-                        Console.WriteLine("Overall mood: Neutral");
-            break;
-        case "3":
-            int totalCharacters = text.Length;
-                int letterCount = 0;
-                int digitCount = 0;
-                int whitespaceCount = 0;
-
-                foreach (char c in text)
+            foreach (string word in textWords)
+            {
+                foreach (string positive in positiveWords)
                 {
-                    if (char.IsLetter(c))
-                        letterCount++;
-                    if (char.IsDigit(c))
-                        digitCount++;
-                    if (char.IsWhiteSpace(c))
-                        whitespaceCount++;
+                    if (word == positive)
+                    {
+                        positiveCount++;
+                    }
                 }
-                Console.WriteLine("Total Characters: " + totalCharacters);
-                Console.WriteLine("Total Letters: " + letterCount);
-                Console.WriteLine("Total Digits: " + digitCount);
-                Console.WriteLine("Total Whitespaces: " + whitespaceCount);
-                break;
+
+                foreach (string negative in negativeWords)
+                {
+                    if (word == negative)
+                    {
+                        negativeCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Positive words: " + positiveCount);
+            Console.WriteLine("Negative words: " + negativeCount);
+
+            if (positiveCount > negativeCount)
+            {
+                Console.WriteLine("Overall mood: Positive");
+            }
+            else if (negativeCount > positiveCount)
+            {
+                Console.WriteLine("Overall mood: Negative");
+            }
+            else
+            {
+                Console.WriteLine("Overall mood: Neutral");
+            }
+            break;
+
+        case "3":
+            int letterCount = 0;
+            int digitCount = 0;
+            int whitespaceCount = 0;
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    letterCount++;
+                }
+                else if (char.IsDigit(c))
+                {
+                    digitCount++;
+                }
+                else if (char.IsWhiteSpace(c))
+                {
+                    whitespaceCount++;
+                }
+            }
+
+            Console.WriteLine("Total Letters: " + letterCount);
+            Console.WriteLine("Total Digits: " + digitCount);
+            Console.WriteLine("Total Whitespaces: " + whitespaceCount);
+            break;
+
         case "4":
-            string[] wordArray = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-                    
-                    string[] topWords = new string[5];
-                    int[] topLengths = new int[5];
-                    
-                    for (int i = 0; i < 5; i++)
-                    {
-                        topWords[i] = "";
-                        topLengths[i] = 0;
-                    }
+            string[] wordList = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string longestWord = "";
+            int maxLength = 0;
 
-                    foreach (string word in wordArray)
-                    {
-                        int wordLength = word.Length;
-                        
-                        for (int i = 0; i < 5; i++)
-                        {
-                            if (wordLength > topLengths[i])
-                            {
-                                for (int j = 4; j > i; j--)
-                                {
-                                    topWords[j] = topWords[j - 1];
-                                    topLengths[j] = topLengths[j - 1];
-                                }
-                                topWords[i] = word;
-                                topLengths[i] = wordLength;
-                                break;
-                            }
-                        }
-                    }
+            foreach (string word in wordList)
+            {
+                if (word.Length > maxLength)
+                {
+                    longestWord = word;
+                    maxLength = word.Length;
+                }
+            }
 
-                    Console.WriteLine("Top 5 Longest Words:");
-                    bool hasWords = false;
-                    for (int i = 0; i < 5; i++)
-                    {
-                        if (topLengths[i] > 0)
-                        {
-                            Console.WriteLine($"{i + 1}. {topWords[i]} (Length: {topLengths[i]})");
-                            hasWords = true;
-                        }
-                    }
-                    if (!hasWords)
-                    {
-                        Console.WriteLine("No words found!");
-                    }
-                    break;
+            Console.WriteLine("Longest word: " + longestWord + " (Length: " + maxLength + ")");
+            break;
+
+        case "5":
+            Console.WriteLine("Sayonara!");
+            return;
+
         default:
-            Console.WriteLine("[!] Invalid input, pick a number between 1 and 5.");
+            Console.WriteLine("[!] Invalid option. Try again.");
             break;
     }
 }
