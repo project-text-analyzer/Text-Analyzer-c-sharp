f191d65f1e4ca32b79c6b9942342bab05145deaf
so1icitx <tojitiktok@proton.me>
2025-03-27 19:52:21 +0200

    BIG rework

--- analyzer.cs
+++ analyzer.cs
@@ -9,172 +9,77 @@ string asciiLogo = """""
 
                    """"";
 Console.WriteLine(asciiLogo);
-Console.WriteLine("[!] Welcome to the Sigma Text Analyzer");
+Console.WriteLine("[*] Welcome to the SIGMA Text Analyzer!!");
+
 Console.Write("[!] Enter your text: ");
 string text = Console.ReadLine();
 
-if (text.Length == 0 || text == " ")
+if (text == "")
 {
-    Console.WriteLine("[!] [ERROR]: No text! Bye!");
+    Console.WriteLine("[!] ERROR no text provided exiting.....");
     return;
 }
 
-string[] history = new string[5]; 
+string[] history = new string[5];
 int historyCount = 0;
-history[historyCount++] = text;
-
+history[historyCount] = text;
+historyCount += 1;
 
 string[] positiveWords = { "happy", "great", "awesome", "good", "excellent", "wonderful", "amazing", "beautiful", "love", "joy", "fantastic", "perfect", "nice", "lovely", "exciting", "brilliant", "super", "cool", "fun", "glad", "positive", "success", "win", "cheerful", "delight", "pleasure", "hope" };
 string[] negativeWords = { "sad", "bad", "terrible", "awful", "horrible", "depressing", "angry", "hate", "miserable", "poor", "dreadful", "pain", "sucks", "stupid", "ugly", "boring", "annoying", "mad", "furious", "fail", "negative", "stress", "anxiety", "fear", "disappoint", "upset", "hurt" };
 
-
-void CountWords(out int count)
-{
-    count = 1;
-    for (int i = 0; i < text.Length; i++)
-        if (text[i] == ' ') count++;
-    if (text[0] == ' ' || text[text.Length - 1] == ' ') count--;
-    Console.WriteLine("[*] Words: " + count);
-}
-
-void CheckMood(out int pos, out int neg, out string mood)
-{
-    pos = 0; neg = 0;
-    string word = "";
-    string lowerText = text.ToLower(); 
-    for (int i = 0; i < lowerText.Length; i++)
-    {
-        if (lowerText[i] != ' ') word += lowerText[i];
-        if (lowerText[i] == ' ' || i == lowerText.Length - 1)
-        {
-            for (int j = 0; j < positiveWords.Length; j++)
-                if (word == positiveWords[j]) pos++;
-            for (int j = 0; j < negativeWords.Length; j++)
-                if (word == negativeWords[j]) neg++;
-            word = "";
-        }
-    }
-    mood = pos > neg ? "Positive" : neg > pos ? "Negative" : "Neutral";
-    Console.WriteLine("[*] Positive: " + pos + ", Negative: " + neg + ", Mood: " + mood);
-}
-
-void TextStats(out int letters, out int digits, out int spaces)
-{
-    letters = 0; digits = 0; spaces = 0;
-    for (int i = 0; i < text.Length; i++)
-    {
-        char c = text[i];
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) letters++;
-        else if (c >= '0' && c <= '9') digits++;
-        else if (c == ' ') spaces++;
-    }
-    Console.WriteLine("[*] Letters: " + letters + ", Digits: " + digits + ", Spaces: " + spaces);
-}
-
-void TopThreeWords(out string[] words, out int[] lengths)
-{
-    words = new string[] { "", "", "" };
-    lengths = new int[] { 0, 0, 0 };
-    string word = "", textEnd = text + " ";
-    for (int i = 0; i < textEnd.Length; i++)
-    {
-        if (textEnd[i] != ' ') word += textEnd[i];
-        else if (word != "")
-        {
-            int len = word.Length;
-            if (len > lengths[0])
-            {
-                lengths[2] = lengths[1]; words[2] = words[1];
-                lengths[1] = lengths[0]; words[1] = words[0];
-                lengths[0] = len; words[0] = word;
-            }
-            else if (len > lengths[1])
-            {
-                lengths[2] = lengths[1]; words[2] = words[1];
-                lengths[1] = len; words[1] = word;
-            }
-            else if (len > lengths[2])
-            {
-                lengths[2] = len; words[2] = word;
-            }
-            word = "";
-        }
-    }
-    Console.WriteLine("[*] Top 3 Words:");
-    for (int i = 0; i < 3; i++)
-        if (words[i] != "") Console.WriteLine("[*] " + (i + 1) + ": '" + words[i] + "' (" + lengths[i] + ")");
-}
-
-void ShowHistory()
-{
-    Console.WriteLine("[*] History:");
-    for (int i = 0; i < historyCount; i++)
-        if (history[i] != null) Console.WriteLine("[*] " + (i + 1) + ": '" + history[i] + "'");
-}
-
-void SaveResults()
-{
-    Console.Write("[!] Enter file name (e.g., 'output.txt'): ");
-    string fileName = Console.ReadLine();
-    if (fileName == "") fileName = "results.txt";
-
-    string results = "Analyzer Results\nText: " + text + "\n";
-    
-    int count; CountWords(out count);
-    results += "Words: " + count + "\n";
-
-    int pos, neg; string mood;
-    CheckMood(out pos, out neg, out mood);
-    results += "Positive: " + pos + ", Negative: " + neg + ", Mood: " + mood + "\n";
-
-    int letters, digits, spaces;
-    TextStats(out letters, out digits, out spaces);
-    results += "Letters: " + letters + ", Digits: " + digits + ", Spaces: " + spaces + "\n";
-
-    string[] words; int[] lengths;
-    TopThreeWords(out words, out lengths);
-    results += "Top 3 Words:\n";
-    for (int i = 0; i < 3; i++)
-        if (words[i] != "") results += (i + 1) + ": '" + words[i] + "' (" + lengths[i] + ")\n";
-
-    results += "History:\n";
-    for (int i = 0; i < historyCount; i++)
-        if (history[i] != null) results += (i + 1) + ": '" + history[i] + "'\n";
-
-    System.IO.File.WriteAllText(fileName, results);
-    Console.WriteLine("[!] Saved to '" + fileName + "'");
-}
-
 while (true)
 {
-    Console.WriteLine("\n=== Menu ===");
+    Console.WriteLine("");
+    Console.WriteLine("=== Menu ===");
     Console.WriteLine("1 - Count Words");
-    Console.WriteLine("2 - Check Mood");
+    Console.WriteLine("2 - Mood Check");
     Console.WriteLine("3 - Text Stats");
-    Console.WriteLine("4 - Top 3 Words");
+    Console.WriteLine("4 - Top 3 Longest Words");
     Console.WriteLine("5 - Show History");
     Console.WriteLine("6 - Save Results");
     Console.WriteLine("7 - Exit");
-    Console.Write("[!] Pick (1-7): ");
+    Console.Write("[!] Pick a choice: ");
 
     string choice = Console.ReadLine();
+
     switch (choice)
     {
-        case "1": int c; CountWords(out c); 
+        case "1":
+            string wordResult = WordCount(text);
+            Console.WriteLine(wordResult);
             break;
-        case "2": int p, n; string m; CheckMood(out p, out n, out m);
+
+        case "2":
+            string moodResult = MoodCheck(text, positiveWords, negativeWords);
+            Console.WriteLine(moodResult);
             break;
-        case "3": int l, d, s; TextStats(out l, out d, out s); 
+
+        case "3":
+            string statsResult = TextStats(text);
+            Console.WriteLine(statsResult);
             break;
-        case "4": string[] w; int[] len; TopThreeWords(out w, out len); 
+
+        case "4":
+            string top3Result = Top3Words(text);
+            Console.WriteLine(top3Result);
             break;
-        case "5": ShowHistory(); 
+
+        case "5":
+            string historyResult = ShowHistory(history, historyCount);
+            Console.WriteLine(historyResult);
             break;
-        case "6": SaveResults(); 
+
+        case "6":
+            SaveResults(text, positiveWords, negativeWords, history, historyCount);
             break;
-        case "7": Console.WriteLine("[!] Sayonara!"); 
+
+        case "7":
+            Console.WriteLine("[!] Bye bye!");
             return;
-        default: Console.WriteLine("[!] [ERROR]: Pick 1-7!"); 
+
+        default:
+            Console.WriteLine("[!] ERROR, pick 1 to 7!");
             break;
     }
 
@@ -182,10 +87,212 @@ while (true)
     {
         Console.Write("[!] New text (or Enter to keep '" + text + "'): ");
         string newText = Console.ReadLine();
+
         if (newText != "")
         {
             text = newText;
-            if (historyCount < 5) history[historyCount++] = text;
+
+            if (historyCount < 5)
+            {
+                history[historyCount] = text;
+                historyCount = historyCount + 1;
+            }
         }
     }
+}
+
+string WordCount(string text)
+{
+    string[] words = text.Split(' ');
+    int count = 0;
+
+    foreach (string word in words)
+    {
+        if (word != "")
+        {
+            count = count + 1;
+        }
+    }
+
+    return "Word Counter: " + count;
+}
+
+string MoodCheck(string text, string[] positiveWords, string[] negativeWords)
+{
+    int posCount = 0;
+    int negCount = 0;
+    string lowerText = text.ToLower();
+    string[] words = lowerText.Split(' ');
+
+    foreach (string word in words)
+    {
+        if (word != "")
+        {
+            foreach (string posWord in positiveWords)
+            {
+                if (word == posWord)
+                {
+                    posCount = posCount + 1;
+                }
+            }
+
+            foreach (string negWord in negativeWords)
+            {
+                if (word == negWord)
+                {
+                    negCount = negCount + 1;
+                }
+            }
+        }
+    }
+
+    if (posCount > negCount)
+    {
+        return "The text is Positive!";
+    }
+    else if (negCount > posCount)
+    {
+        return "The text is Negative!";
+    }
+    else
+    {
+        return "The text is Neutral..";
+    }
+}
+
+string TextStats(string text)
+{
+    int spaceCount = 0;
+    int letterCount = 0;
+    int digitCount = 0;
+
+    foreach (char c in text)
+    {
+        if (c == ' ')
+        {
+            spaceCount = spaceCount + 1;
+        }
+        else if (char.IsLetter(c))
+        {
+            letterCount = letterCount + 1;
+        }
+        else if (char.IsDigit(c))
+        {
+            digitCount = digitCount + 1;
+        }
+    }
+
+    string result = "";
+    result = result + "Spaces: " + spaceCount + "\n";
+    result = result + "Letters: " + letterCount + "\n";
+    result = result + "Digits: " + digitCount;
+    return result;
+}
+
+string Top3Words(string text)
+{
+    string[] words = text.Split(' ');
+    string[] topWords = new string[3];
+    int[] topLengths = new int[3];
+
+    topWords[0] = "";
+    topWords[1] = "";
+    topWords[2] = "";
+    topLengths[0] = 0;
+    topLengths[1] = 0;
+    topLengths[2] = 0;
+
+    foreach (string word in words)
+    {
+        if (word != "")
+        {
+            int len = word.Length;
+
+            if (len > topLengths[0])
+            {
+                topLengths[2] = topLengths[1];
+                topWords[2] = topWords[1];
+                topLengths[1] = topLengths[0];
+                topWords[1] = topWords[0];
+                topLengths[0] = len;
+                topWords[0] = word;
+            }
+            else if (len > topLengths[1])
+            {
+                topLengths[2] = topLengths[1];
+                topWords[2] = topWords[1];
+                topLengths[1] = len;
+                topWords[1] = word;
+            }
+            else if (len > topLengths[2])
+            {
+                topLengths[2] = len;
+                topWords[2] = word;
+            }
+        }
+    }
+
+    string result = "";
+    if (topWords[0] != "")
+    {
+        result = result + "1: " + topWords[0] + ", length: " + topLengths[0] + "\n";
+    }
+    if (topWords[1] != "")
+    {
+        result = result + "2: " + topWords[1] + ", length: " + topLengths[1] + "\n";
+    }
+    if (topWords[2] != "")
+    {
+        result = result + "3: " + topWords[2] + ", length: " + topLengths[2];
+    }
+    return result;
+}
+
+string ShowHistory(string[] history, int historyCount)
+{
+    string result = "";
+    result = result + "[*] History:\n";
+
+    for (int i = 0; i < historyCount; i++)
+    {
+        if (history[i] != null)
+        {
+            result = result + "[*] " + (i + 1) + ": '" + history[i] + "'\n";
+        }
+    }
+
+    return result;
+}
+
+void SaveResults(string text, string[] positiveWords, string[] negativeWords, string[] history, int historyCount)
+{
+    Console.Write("[!] Enter file name (e.g., 'output.txt'): ");
+    string fileName = Console.ReadLine();
+
+    if (fileName == "")
+    {
+        fileName = "results.txt";
+    }
+
+    string results = "";
+    results = results + "Analyzer Results\n";
+    results = results + "Text: " + text + "\n";
+
+    string wordResult = WordCount(text);
+    results = results + wordResult + "\n";
+
+    string moodResult = MoodCheck(text, positiveWords, negativeWords);
+    results = results + moodResult + "\n";
+
+    string statsResult = TextStats(text);
+    results = results + statsResult + "\n";
+
+    string top3Result = Top3Words(text);
+    results = results + top3Result + "\n";
+
+    string historyResult = ShowHistory(history, historyCount);
+    results = results + historyResult;
+
+    System.IO.File.WriteAllText(fileName, results);
+    Console.WriteLine("[!] Saved to '" + fileName + "'");
 }
